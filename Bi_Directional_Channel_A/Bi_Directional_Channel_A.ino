e100a0ff3fb905c18fe870ea11b4565e131c6f27
ScottReynolds95 <s.reynolds@students.rave.ac.uk>
2018-01-30 14:00:29 +0000

    Transciever Bi Directional
    changed code found at
    http://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
    to support bi directional led communication

--- Bi_Directional_Channel_A/Bi_Directional_Channel_A.ino
+++ Bi_Directional_Channel_A/Bi_Directional_Channel_A.ino
@@ -9,12 +9,15 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define led 12
+#define button 3
+#define led 2
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 boolean buttonState = 0;
+boolean button1State = 0;
 void setup() {
-  pinMode(12, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(2, OUTPUT);
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00001
   radio.openReadingPipe(1, addresses[0]); // 00002
@@ -23,17 +26,19 @@ void setup() {
 void loop() {
   delay(5);
   radio.stopListening();
-  int potValue = analogRead(A0);
-  int angleValue = map(potValue, 0, 1023, 0, 180);
-  radio.write(&angleValue, sizeof(angleValue));
+  buttonState = digitalRead(button);
+  radio.write(&buttonState, sizeof(buttonState));
   delay(5);
   radio.startListening();
   while (!radio.available());
-  radio.read(&buttonState, sizeof(buttonState));
-  if (buttonState == HIGH) {
+  radio.read(&button1State, sizeof(button1State));
+  if (button1State == HIGH) {
     digitalWrite(led, HIGH);
   }
   else {
     digitalWrite(led, LOW);
   }
 }
+//  int potValue = analogRead(A0);
+//  int angleValue = map(potValue, 0, 1023, 0, 180);
+//  radio.write(&angleValue, sizeof(angleValue));
