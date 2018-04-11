db49eeeb4d7915ff0bc57afc53f3da2d337b8ca2
ScottReynolds95 <s.reynolds@students.rave.ac.uk>
2018-04-11 10:58:17 +0100

    updatesd code to remove lcd screen and just talk

---
+++ transciever_with_address/transciever_with_address.ino
@@ -0,0 +1,135 @@
+// rf69 demo tx rx oled.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple addressed, reliable messaging client
+// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
+// reliability, so you should only use RH_RF69  if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf69_server.
+// Demonstrates the use of AES encryption, setting the frequency and modem 
+// configuration
+
+#include <SPI.h>
+#include <Wire.h>
+#include <RHReliableDatagram.h>
+#include <RH_RF69.h>
+
+// Where to send packets to!
+#define DEST_ADDRESS   1
+
+// change addresses for each client board, any number :)
+#define MY_ADDRESS     2
+
+/************ OLED Setup ***************/
+Adafruit_SSD1306 oled = Adafruit_SSD1306();
+
+/************ Radio Setup ***************/
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF69_FREQ 915.0
+
+
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+#endif
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void setup() 
+{
+
+  Serial.begin(115200);
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+
+
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+
+  Serial.println("Feather RFM69 RX/TX Test!");
+
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  
+  if (!rf69_manager.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(14, true);
+  
+  pinMode(LED, OUTPUT);
+
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+
+}
+
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+uint8_t data[] = "  OK";
+
+void loop()
+{  
+   if (rf69_manager.available()) {
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (rf69_manager.recvfromAck(buf, &len, &from))
+    {
+      buf[len] = 0;
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: "); Serial.println(rf69.lastRssi(), DEC);
+
+      // Send a reply back to the originator client
+      if (!rf69_manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("sendtoWait failed");
+    
+    }
+  }
+
+    delay(5000);  // Wait 5 second between transmits, could also 'sleep' here!
+   
+    char radiopacket[20] = "Hello World #";
+    itoa(packetnum++, radiopacket+13, 10);
+    Serial.print("Sending "); Serial.println(radiopacket);
+
+    if (rf69_manager.sendtoWait((uint8_t *)radiopacket, strlen(radiopacket), DEST_ADDRESS)) {
+      // Now wait for a reply from the server
+      uint8_t len = sizeof(buf);
+      uint8_t from;   
+      if (rf69_manager.recvfromAckTimeout(buf, &len, 2000, &from)) {
+        buf[len] = 0;
+        Serial.print("Got reply from #");
+        Serial.print(from); Serial.print(": ");
+        Serial.println((char*)buf);
+        
+      } else {
+        Serial.println("No reply, is anyone listening?");
+      }
+    } else {
+      Serial.println("sendtoWait failed");
+    }
+  }
+
