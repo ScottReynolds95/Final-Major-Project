1cd3c15af35502b463da957b8cb51a3e341fd1d2
ScottReynolds95 <s.reynolds@students.rave.ac.uk>
2018-06-02 22:32:29 +0100

    final hand in testing process

--- transmitter_working_with_board/transmitter_working_with_board.ino
+++ transmitter_working_with_board/transmitter_working_with_board.ino
@@ -21,19 +21,19 @@
 // change addresses for each client board, any number :)
 #define MY_ADDRESS     2
 
-//#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
-//  #define RFM69_INT     3  // 
-//  #define RFM69_CS      4  //
-//  #define RFM69_RST     2  // "A"
-//  #define LED           8
-//#endif
-
-#if defined (__AVR_ATmega328P__)  // trinket
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
   #define RFM69_INT     3  // 
-  #define RFM69_CS      5  //
-  #define RFM69_RST     4  // "A"
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
   #define LED           13
 #endif
+//
+//#if defined (__AVR_ATmega328P__)  // trinket
+//  #define RFM69_INT     3  // 
+//  #define RFM69_CS      5  //
+//  #define RFM69_RST     4  // "A"
+//  #define LED           13
+//#endif
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
@@ -74,7 +74,7 @@ void setup()
 
   // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
   // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  rf69.setTxPower(14, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
   // The encryption key has to be the same as the one in the server
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
