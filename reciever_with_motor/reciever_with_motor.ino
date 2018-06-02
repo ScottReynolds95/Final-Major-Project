eeae18545920c47b10fbebea2c6b4dfaa48e160b
ScottReynolds95 <s.reynolds@students.rave.ac.uk>
2018-06-02 22:48:05 +0100

    demo
    demo code

--- reciever_with_motor/reciever_with_motor.ino
+++ reciever_with_motor/reciever_with_motor.ino
@@ -1,13 +1,7 @@
-// rf69 demo tx rx.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
-// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
-// reliability, so you should only use RH_RF69  if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
-// configuration
-
+// This software is a manipualtion of the LowPowerLab's RFM69 recomended on the adafuit documention for the transceivers
+// found at https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/using-the-rfm69-radio
+// I also used example code from the Adafruit_DRV2605 Library to control the haptic driver
+// found at https://learn.adafruit.com/adafruit-drv2605-haptic-controller-breakout/arduino-code#
 #include <SPI.h>
 #include <RH_RF69.h>
 #include <RHReliableDatagram.h>
@@ -15,21 +9,14 @@
 #include "Adafruit_DRV2605.h"
 /************ Radio Setup ***************/
 
-// Change to 434.0 or other frequency, must match RX's freq!
+
 #define RF69_FREQ 915.0
 
-// who am i? (server address)
+// Common Ground Unit Address
 #define MY_ADDRESS     1
 
 
-//#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
-//  #define RFM69_INT     3  // 
-//  #define RFM69_CS      4  //
-//  #define RFM69_RST     2  // "A"
-//  #define LED           8
-//#endif
-
-#if defined (__AVR_ATmega328P__)  // trinket
+#if defined (__AVR_ATmega328P__)  // trinket pinout 
   #define RFM69_INT     3  // 
   #define RFM69_CS      5  //
   #define RFM69_RST     4  // "A"
@@ -45,24 +32,17 @@ RH_RF69 rf69(RFM69_CS, RFM69_INT);
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
 
-
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void setup() 
 {
   Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
-
   drv.begin();
-  
-  drv.selectLibrary(1);
+  drv.selectLibrary(1); // Haptic
   
   // I2C trigger by sending 'go' command 
   // default, internal trigger when sending GO command
-  drv.setMode(DRV2605_MODE_INTTRIG); 
-
-  
-  pinMode(LED, OUTPUT);     
+  drv.setMode(DRV2605_MODE_INTTRIG);  
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
@@ -80,17 +60,14 @@ void setup()
     while (1);
   }
   Serial.println("RFM69 radio init OK!");
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
+
   if (!rf69.setFrequency(RF69_FREQ)) {
     Serial.println("setFrequency failed");
   }
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  rf69.setTxPower(20, true);  // highest power
 
-  // The encryption key has to be the same as the one in the server
+  // The encryption key - user protection
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
@@ -106,7 +83,7 @@ uint8_t data[] = "And hello back to you";
 // Dont put this on the stack:
 uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
 
-uint8_t effect = 70;// motor effect
+uint8_t effect = 70;// Transition Ramp Down Long Smooth 1
 
 void loop() {
   if (rf69_manager.available())
@@ -126,14 +103,11 @@ void loop() {
       drv.setWaveform(0, effect);  // play effect 
       drv.setWaveform(1, 0);       // end waveform
 
-  // play the effect!
+  // Play Common Ground Output!
       drv.go();
 
-  // wait a bit
   delay(1000);
-//      Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
 
-      // Send a reply back to the originator client
       if (!rf69_manager.sendtoWait(data, sizeof(data), from))
         Serial.println("Sending failed (no ack)");
     }
@@ -141,11 +115,4 @@ void loop() {
 }
 
 
-//void Blink(byte PIN, byte DELAY_MS, byte loops) {
-//  for (byte i=0; i<loops; i++)  {
-//    digitalWrite(PIN,HIGH);
-//    delay(DELAY_MS);
-//    digitalWrite(PIN,LOW);
-//    delay(DELAY_MS);
-//  }
-//}
+
